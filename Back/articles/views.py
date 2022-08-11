54e4b7ed83116887e3649a8bf5dea6500fa26a00
Bombozaur666 <bombozaur6@gmail.com>
2022-08-12 01:06:41 +0200

    IW-9 simple return comments

--- Back/articles/views.py
+++ Back/articles/views.py
@@ -8,20 +8,25 @@ from rest_framework.views import APIView
 # Create your views here.
 class ArticleView(APIView):
     def get(self, request, pk):
+        #return article with comments
         article = Article.objects.published().filter(pk=pk)
         article_serializer = ArticleSerializer(article, many=True)
         comments = Comment.objects.filter(article_id=pk)
         comments_serializer = CommentSerializer(comments, many=True)
         return JsonResponse([article_serializer.data, comments_serializer.data], safe=False)
+    def post(self, request, pk):
+        #add comment to specific article
 
 
 class ArticlesListView(APIView):
 
     def get(self, request):
+        #return all articles
         request.GET
         articles = Article.objects.published().all()
         serializer = ArticleSerializer(articles, many=True)
         return JsonResponse(serializer.data, safe=False)
 
     def post(self, request):
+
         pass
