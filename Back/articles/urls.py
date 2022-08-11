27bb3d580891ccbdbfb17edecf084d001e1b695a
Bombozaur666 <33629581+Bombozaur666@users.noreply.github.com>
2022-08-12 00:13:27 +0200

    IW-11 add feed RSS (#4)

--- Back/articles/urls.py
+++ Back/articles/urls.py
@@ -3,11 +3,9 @@ app_name = 'articles'
 from django.urls import path
 from .views import ArticlesListView, ArticleView
 
-"""router = DefaultRouter()
-router.register('', )
-router.register('', )"""
 
 urlpatterns = [
     path('', ArticlesListView.as_view(), name='articles_list'),
     path('<int:pk>/', ArticleView.as_view(), name='article'),
+    path('feed/', LatestArticles(), name='articles_feed'),
 ]
