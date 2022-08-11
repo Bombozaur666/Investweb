508fd9cd2b24810b4c16491612f74df19d7787bd
Bombozaur666 <bombozaur6@gmail.com>
2022-08-12 01:04:06 +0200

    IW-9 simple return comments

--- Back/articles/views.py
+++ Back/articles/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import viewsets
 from .models import Article, Comment
 from .serializers import ArticleSerializer, CommentSerializer
+from rest_framework.views import APIView
 
 
 # Create your views here.
-class BasicArticle(viewsets.ModelViewSet):
-    queryset = Article.objects.published()
-    serializer_class = ArticleSerializer
+class ArticleView(APIView):
+    def get(self, request, pk):
+        article = Article.objects.published().filter(pk=pk)
+        article_serializer = ArticleSerializer(article, many=True)
+        comments = Comment.objects.filter(article_id=pk)
+        comments_serializer = CommentSerializer(comments, many=True)
+        return JsonResponse([article_serializer.data, comments_serializer.data], safe=False)
 
 
-class BasicComment(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+class ArticlesListView(APIView):
+
+    def get(self, request):
+        request.GET
+        articles = Article.objects.published().all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    def post(self, request):
+        pass
