8b49db43555316a3862044f648cf9fb44342aeee
Bombozaur666 <bombozaur6@gmail.com>
2022-08-11 12:23:47 +0200

    IW-5 simple serializer

--- Back/articles/serializers.py
+++ Back/articles/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Article
 from taggit_serializer.serializers import (TagListSerializerField,
                                            TaggitSerializer)
-
+# addon to work properly you must find
+# 'from django.utils.translation import ugettext_lazy as _'
+# and replace it with
+# 'from django.utils.translation import gettext_lazy as _'
+# because django in 4.x version removed old functions and libraries didn't get update
 
 class ArticleSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField()
