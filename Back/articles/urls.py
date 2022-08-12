19cae920dc0b392fbe3153ac187a85a54fd5f6ff
Bombozaur666 <bombozaur6@gmail.com>
2022-08-12 21:28:25 +0200

    IW-9 simple

--- Back/articles/urls.py
+++ Back/articles/urls.py
@@ -1,14 +1,17 @@
 app_name = 'articles'
-from .views import ArticleView, ArticlesListView, ArticleTypeView, FiltersView, ArticleTypeWithLangView
+from .views import ArticleView, ArticlesListView, ArticleTypeView, FiltersView, ArticleTypeWithLangView, PolishArticlesView, EnglishArticlesView, GermanArticlesView
 from .feeds import LatestArticles
 from django.urls import path
 
 urlpatterns = [
     path('', ArticlesListView.as_view()),
     path('<int:pk>/', ArticleView.as_view(), name='article'),
-    path('feed/', LatestArticles(), name='articles_feed'),
-    path('filters/', FiltersView.as_view(), name='filters_view'),
-    path('<str:types>/', ArticleTypeView.as_view(), name='type_view'),
-    path('<str:types>/<str:lang>/', ArticleTypeWithLangView.as_view(), name='type_view'),
+    path('pl/', PolishArticlesView.as_view(), name='polish-articles'),
+    path('eng/', EnglishArticlesView.as_view(), name='english-articles'),
+    path('de/', GermanArticlesView.as_view(), name='german-articles'),
+    path('feed/', LatestArticles(), name='articles-feed'),
+    path('filters/', FiltersView.as_view(), name='filters-view'),
+    path('<str:types>/', ArticleTypeView.as_view(), name='type-view'),
+    path('<str:types>/<str:lang>/', ArticleTypeWithLangView.as_view(), name='type-view'),
 
 ]
